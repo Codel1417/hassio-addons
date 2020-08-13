aeef39eeb41b522cca0d12404806218dbce0d1b0
Codel1417 <austinrichards98@gmail.com>
2020-08-12 19:13:55 -0700

    Update Run.sh

--- jackett/run.sh
+++ jackett/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./Jackett.Server/bin/Debug/netcoreapp3.1/linux-x64/jackett --DataFolder /data --NoUpdates 
+~/Jackett/src/Jackett.Server/bin/Debug/netcoreapp3.1/linux-x64/jackett --DataFolder /data --NoUpdates 
