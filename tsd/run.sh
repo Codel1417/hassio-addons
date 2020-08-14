ca09f229a83112b4afa701a1ef8e460fd899c480
Codel1417 <austinrichards98@gmail.com>
2020-08-13 17:26:20 -0700

    Update run.sh

--- tsd/run.sh
+++ tsd/run.sh
@@ -1,4 +1,5 @@
-#!//usr/local/bin/docker-entrypoint.sh
+#!/bin/bash
 # https://stackoverflow.com/questions/32076878/logging-solution-for-multiple-containers-running-on-same-host
+dockered
 cd ~/TheSpaghettiDetective
 docker-compose up
