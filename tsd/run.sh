387a4c19899130285f6c455dac7e61059681b197
Codel1417 <austinrichards98@gmail.com>
2020-08-13 17:13:53 -0700

    Update

--- tsd/run.sh
+++ tsd/run.sh
@@ -1,38 +1,3 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/32076878/logging-solution-for-multiple-containers-running-on-same-host
-dockerd & 
-sleep 30
-cd ~/TheSpaghettiDetective 
-docker-compose up -d
-
-names=$(docker ps --format "{{.Names}}")
-echo "tailing $names"
-
-while read -r name
-do
-  # eval to show container name in jobs list
-  eval "docker logs -f --tail=5 \"$name\" | sed -e \"s/^/[-- $name --] /\" &"
-  # For Ubuntu 16.04
-  #eval "docker logs -f --tail=5 \"$name\" |& sed -e \"s/^/[-- $name --] /\" &"
-done <<< "$names"
-
-function _exit {
-  echo
-  echo "Stopping tails $(jobs -p | tr '\n' ' ')"
-  echo "..."
-
-  # Using `sh -c` so that if some have exited, that error will
-  # not prevent further tails from being killed.
-  jobs -p | tr '\n' ' ' | xargs -I % sh -c "kill % || true"
-
-  echo "Done"
-}
-
-# On ctrl+c, kill all tails started by this script.
-trap _exit EXIT
-
-# For Ubuntu 16.04
-#trap _exit INT
-
-# Don't exit this script until ctrl+c or all tails exit.
-wait
+dockerd-entrypoint.sh "cd ~/TheSpaghettiDetective && docker-compose up"
