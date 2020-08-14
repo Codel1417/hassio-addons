86aee5952a3760bcbf623532451adfa0bdeff83d
Codel1417 <austinrichards98@gmail.com>
2020-08-13 17:30:55 -0700

    Update run.sh

--- tsd/run.sh
+++ tsd/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/32076878/logging-solution-for-multiple-containers-running-on-same-host
-dockered
+dockerd --host=unix:///var/run/shared/docker.sock
 cd ~/TheSpaghettiDetective
 docker-compose up
