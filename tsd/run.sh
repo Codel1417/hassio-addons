f8802411dec7b22104ea7a6214d526a18ac26523
Codel1417 <austinrichards98@gmail.com>
2020-08-27 05:28:08 -0700

    update tsd

--- tsd/run.sh
+++ tsd/run.sh
@@ -2,10 +2,10 @@
 # https://stackoverflow.com/questions/32076878/logging-solution-for-multiple-containers-running-on-same-host
 
 export DOCKER_HOST=unix:///var/run/docker.sock
-dockerd --host=unix:///var/run/docker.sock > /dev/null &
+dockerd --host=unix:///var/run/docker.sock &
 pid=$!
 cd ~/TheSpaghettiDetective
-docker-compose up
+docker-compose start
 
 while true
 do
