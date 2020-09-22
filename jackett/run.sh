65f22e36c51f25d4a3bce149ef67d282b2c9f224
Codel1417 <austinrichards98@gmail.com>
2020-09-21 22:58:21 -0700

    Remove log

--- jackett/run.sh
+++ jackett/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+rm /data/log.txt || true
+ln -s /dev/null /data/log.txt
 
 ~/Jackett/src/Jackett.Server/bin/Debug/netcoreapp3.1/linux-x64/jackett --DataFolder /data --NoUpdates 
